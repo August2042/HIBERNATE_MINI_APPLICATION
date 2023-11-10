b166314d6e9879683e0d3912665a578cc5e1fd6b
August Kumar <94859002+August2042@users.noreply.github.com>
2023-11-10 12:37:00 +0530

    addition of getStudentList and updateStudent

--- daoImpPackage/StudentDaoImpl.java
+++ daoImpPackage/StudentDaoImpl.java
@@ -1,7 +1,9 @@
 package daoImpPackage;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -9,6 +11,7 @@ import daoPackage.StudentDao;
 import hibernateConfigurationPackage.HibernateConfiguration;
 import javaBeans.Student;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class StudentDaoImpl implements StudentDao {
 
@@ -66,8 +69,46 @@ public class StudentDaoImpl implements StudentDao {
 
 	@Override
 	public List<Student> getStudent() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Student> students = new ArrayList<>();
+		SessionFactory sessionFactory = HibernateConfiguration.getInstance().getSessionFactory();
+		Session session = sessionFactory.openSession();
+
+		try {
+			// Use HQL (Hibernate Query Language) to retrieve all students
+			Query<Student> query = session.createQuery("FROM Student", Student.class);
+			students = query.list();
+		} catch (HibernateException e) {
+			// Handle Hibernate exceptions
+			e.printStackTrace();
+		} finally {
+			// Close the session in a finally block to ensure it gets closed
+			if (session != null && session.isOpen()) {
+				session.close();
+			}
+		}
+
+		return students;
+	}
+
+	@Override
+	public Boolean updateStudent(Student student) {
+		SessionFactory sessionFactory = HibernateConfiguration.getInstance().getSessionFactory();
+		Session session = sessionFactory.openSession();
+
+		try {
+			session.beginTransaction();
+			session.update(student);
+			session.getTransaction().commit();
+			return true;
+		} catch (Exception e) {
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+			e.printStackTrace(); // You might want to handle the exception more gracefully in a real application
+			return false;
+		} finally {
+			session.close();
+		}
 	}
 
 }
